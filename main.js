9ff9ca004c99ff60407f9a85ab7f348e2248eae2
jacksombrbs <jacksom.barbosa@escola.pr.gov.br>
2025-07-30 19:54:22 -0300

    Att 6

--- main.js
+++ main.js
@@ -51,8 +51,7 @@ function geraSenha() {
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
-  
+
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
